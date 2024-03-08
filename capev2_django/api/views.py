b0faf313ff750f513192737dd37cc6ea2aa6509f
Ubuntu <azureuser@demo-docker.1con4zil5ejeri3y5typ2ynhpc.bx.internal.cloudapp.net>
2024-03-08 18:59:10 +0000

    update analyze_file

--- capev2_django/api/views.py
+++ capev2_django/api/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from django.http import HttpResponseServerError
+import requests
 
 # Create your views here.
+def analyze_file(request, *args, **kwargs):
+    url = 'https://capesandbox.com/apiv2/analyze_file'  # Example API endpoint URL
+    headers = {'Authorization': 'Token d629af8a5ea6f92294d887019e7fccca554bb109'}
+
+    if request.method == 'POST':
+        file = request.FILES['file']
+        files = {'file': file}
+        response = requests.post(url, headers=headers, files=files)
+
+        if response.status_code == 200:
+            # Handle successful response
+            data = response.json()
+            return JsonResponse(data)
+        else:
+            # Handle error response
+            return HttpResponseServerError("Failed to analyze file")
--- py_client/basic.py
+++ py_client/basic.py
@@ -1,9 +1,11 @@
 import requests
 
-endpoint = "http://localhost:8000/" #"http://127.0.0.1:8000/"
+endpoint = "http://localhost:8000/api/" #"http://127.0.0.1:8000/"
 
 get_response = requests.get(endpoint)
 
 print(get_response.text)
 print(get_response.status_code)
+print(get_response.headers)
+print(get_response.json())
 
